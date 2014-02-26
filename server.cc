ff2989619cb841484f651c2ce74ad634f87d3ba4
Daniel Seita <14dts1@beja.cs.williams.edu>
2014-02-25 21:33:30 -0500

    random stuff

--- server.cc
+++ server.cc
@@ -21,23 +21,26 @@ int main(int argc, char **argv) {
   int sock = socket(domain, type, protocol);
   cout << "sock = " << sock << endl;
 
+  // Now allow socket reuse
+  // if (setsockopt(sock, ) < 0) exit(1);
+
   // Bind 
   struct sockaddr_in myaddr, remote;
-  myaddr.sin_port = htons(8899);
+  myaddr.sin_port = htons(8101);
   myaddr.sin_family = AF_INET;
   myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
   if(bind(sock, (struct sockaddr*)&myaddr, sizeof(myaddr)) < 0) exit(1);
-  cout << "bind succeeded" << endl;
+  // cout << "bind succeeded" << endl;
 
   // Listen
   int backlog = 100;
   if(listen(sock, backlog) < 0) exit(1);
-  cout << "listen succeeded" << endl;
+  // cout << "listen succeeded" << endl;
 
   // Continually listen to new requests and spawn new threads for each one
   while(true){
     unsigned int remotelen = sizeof(remote);
-    cout << "here" << endl;
+    // cout << "here" << endl;
     int temp_sock = accept(sock, (struct sockaddr*)&remote, &remotelen);
     cout << "accept a connection (before creating the thread), temp_sock = " << temp_sock << endl;
     pthread_t thread;
@@ -72,5 +75,28 @@ void *manage_conn(void *ptr) {
       write(temp_sock, buf, nread);
     close (fd);
   }
+}
 
+/*
+  string document_root = "~/cs339/csci339_web_server";
+  string request = "GET /index.html HTTP/1.0";
+  string rel_file_path, abs_file_path;
+  int space_index;
+
+  if (request.find("GET") == 0 and request.size() >= 4) {
+    
+    // The request string starts with "GET".
+    request = request.substr(4);
+    cout << "after taking out \'GET\', request = \'" << request << "\'" << endl;
+
+    // Parse the file name
+    if ((space_index = request.find(" ")) > 0 and request.size() >= space_index + 1) {
+        rel_file_path = request.substr(0, space_index);
+        request = request.substr(space_index + 1);
+        abs_file_path = document_root + rel_file_path;
+        cout << "after taking out the rel file path, request = \'" << request << "\'" << endl;
+        cout << "abs_file_path = \'" << abs_file_path << "\'" << endl;
+    }
+  }
+*/ 
 } 
--- string_parser.cc
+++ string_parser.cc
@@ -25,30 +25,25 @@ int main(int argc, char **argv) {
 
 /********************************************************************/ 
 
-  string document_root = "~/339/1_web_server";
-  string request = "GET /INDEX.html HTTP/1.0";
+  string document_root = "~/cs339/csci339_web_server";
+  string request = "GET /index.html HTTP/1.0";
   string rel_file_path, abs_file_path;
   int space_index;
 
   if (request.find("GET") == 0 and request.size() >= 4) {
+    
     // The request string starts with "GET".
     request = request.substr(4);
-
     cout << "after taking out \'GET\', request = \'" << request << "\'" << endl;
-    cout.flush();
 
     // Parse the file name
-    if ((space_index = request.find(" ")) > 0 and
-        request.size() >= space_index + 1) {
-
+    if ((space_index = request.find(" ")) > 0 and request.size() >= space_index + 1) {
         rel_file_path = request.substr(0, space_index);
         request = request.substr(space_index + 1);
         abs_file_path = document_root + rel_file_path;
         cout << "after taking out the rel file path, request = \'" << request << "\'" << endl;
         cout << "abs_file_path = \'" << abs_file_path << "\'" << endl;
-        cout.flush();
     }
-
   }
   
   return 0;
